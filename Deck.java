7ded6f00f4a764fb0f02802f2baadbb03d8a011f
CemBC <119740765+CemBC@users.noreply.github.com>
2023-04-19 14:33:34 +0300

    adding comment lines

--- Deck.java
+++ Deck.java
@@ -8,7 +8,7 @@ public class Deck {
     private String[] faces = {"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
 
 
-    public Deck() {
+    public Deck() {   //deck arrayini for each döngüsüyle oluştur
         int i = 0;
         for(String a : suits) {
             for(String b: faces) {
@@ -17,7 +17,7 @@ public class Deck {
         }
     }
 
-    public void display() {
+    public void display() {                 //for each döngüsünü kullanarak deck'in içindeki indexleri yazdırdım ekstra olarak görsellik için ekleme yaptım
         System.out.println("-THE DECK-");
         System.out.print("[");
         for(String a : deck) {
@@ -27,11 +27,11 @@ public class Deck {
         System.out.println();
     }
 
-    public void shuffle() {
+    public void shuffle() {                      //Collection methodu array'e shuffle atamadığı için Arrays.asList() methoduyla deck arrayini liste gibi gösterdim
         Collections.shuffle(Arrays.asList(deck));
     }
-    public void cut() {
-        Random rd = new Random(System.currentTimeMillis());
+    public void cut() {                                     //destenin en başından rastgele belirlenen indexe kadar olan elemanları bir arraye
+        Random rd = new Random(System.currentTimeMillis()); //kalanları farklı bir arraye aktarıp tam tersi sıralamayla geri assign ettim
         int card = rd.nextInt(1,52);
         System.out.println("Sliced from card = " + card);
         String[] sliced1 = Arrays.copyOfRange(deck,0,card);
