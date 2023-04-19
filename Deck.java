63637df834dfafb2aba2ea57e78a81464ee5516b
CemBC <119740765+CemBC@users.noreply.github.com>
2023-04-19 13:33:31 +0300

    Constructor of the deck
    by using for each loop  created array of deck

--- Deck.java
+++ Deck.java
@@ -1,5 +1,15 @@
 public class Deck {
     private String[] deck = new String[52];
-    private String[] suits =  {};
-    private String[] faces = {};
+    private String[] suits =  {"♠","♣","♥","♦"};
+    private String[] faces = {"A","2","3","4","5","6","7","8","9","T","J","Q","K"};
+
+
+    public Deck() {
+        int i = 0;
+        for(String a : suits) {
+            for(String b: faces) {
+                deck[i++] = a+b;
+            }
+        }
+    }
 }
