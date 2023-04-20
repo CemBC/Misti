eaa9de03e6b611bbc2a5f493d4224a783afe994a
CemBC <119740765+CemBC@users.noreply.github.com>
2023-04-20 18:28:34 +0300

    Update Board.java

--- Board.java
+++ Board.java
@@ -33,7 +33,7 @@ public class Board {
     }
 
     public boolean condition() {
-        try { //Tahtaya bir kart atıldıktan sonra atılan kartın tahtayı alıp alamadığını boolean döndürür
+        try {                                           //Tahtaya bir kart atıldıktan sonra atılan kartın tahtayı alıp alamadığını boolean döndürür
             if (board.get(board.size() - 1).substring(1).equals(board.get(board.size() - 2).substring(1))) {
                 return true;                            //Bot.addtoCache() methodunda condition olarak kullanılacak
             }
